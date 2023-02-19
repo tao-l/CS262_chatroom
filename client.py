1468cae389c5681b418276bce38dfbc181bfab6e
Gary Ma <qmaai@connect.ust.hk>
2023-02-19 15:06:42 -0500

    tested create account, login, logout, delete account, exit program

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ Author: Gary Ma, Tao Lin
 
 import sys
 import socket
+import clientFunction
 from clientFunction import *
 
 
@@ -15,7 +16,7 @@ def user_menu():
     It will keep prompting user for function number until a valid one
     """
     print('''
-        CONNECTED TO MESSAGE SERVER - type the number of a function:
+        {} CONNECTED TO MESSAGE SERVER - type the number of a function:
         (1) Create Account
         (2) Login to Account
         (3) List Accounts
@@ -24,27 +25,56 @@ def user_menu():
         (6) Logout
         (7) Fetch Messages
         (8) Exit Program
-    ''')
-    command = int(input('Input a number of the function >> ').strip())
-    while command not in list(range(1,9)):
-        command = int(input('Input a number of the function >> ').strip())
-    return command
+    '''.format(clientFunction.myname))
+    command = input('Input a number of the function >> ').strip()
+    while command not in [str(i) for i in range(1,9)]:
+        command = input('Input a number of the function >> ').strip()
+    return int(command)
+
+def handle_client_ui():
+    """
+    Catching illegal client actions before sending commands in to server
+    """
+    # get user input in command line
+    menu_number = user_menu()
+    # make sure user login or create account before doing anything else
+    while clientFunction.myname == "" and menu_number not in [LIST_ACCOUNT_UI, LOGIN_ACCOUNT_UI, EXIT_PROGRAM_UI, CREATE_ACCOUNT_UI]:
+        print("Please log in or create account first before doing other operations.")
+        menu_number = user_menu()
+    
+    # no creating account when logged in
+    while clientFunction.myname != "" and menu_number == CREATE_ACCOUNT_UI:
+        print("Please log out to create a new account")
+        menu_number = user_menu()
+
+    # no double log in
+    while clientFunction.myname != "" and menu_number == LOGIN_ACCOUNT_UI:
+        print("Please log out to re login. ")
+        menu_number = user_menu()
+
+    # If client exits, no longer needs to tell the server to log off
+    if menu_number == EXIT_PROGRAM_UI:
+        sys.exit()
+
+    return menu_number
 
 
 if __name__ == "__main__":
-    if len(sys.argv))!=3:
+    if len(sys.argv)!=3:
         print("ERROR: Please use python client.py <host ip> <port>.")
         sys.exit()
-
+    
+    # python client.py host_ip port_number
     # get ip address and ports from command line 
-    host_ip = sys.argv[1]
-    host_port = sys.argv[2]
+    host_ip = str(sys.argv[1])
+    host_port = int(sys.argv[2])
     
     # first while loop. If connection drops in the middle, user are required restart the session because there are no way for the server to tell the user is the same one as before. Such situations happen when the server is down or the network is down for a long time
     # 1) client to server sendall fails
     # 2) client does not hear server's confirmation response, or too long, or server crashes
     while True:
-
+        menu_number = handle_client_ui() 
+        
         # starting a client socket using TCP protocol
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.settimeout(300)
@@ -54,33 +84,23 @@ if __name__ == "__main__":
                 print("Error: Cannot connect to host {} port {}, try again.".format(host_ip, host_port))
                 sys.exit()
 
-            myname = "" # a place holder for self name
-            while True:
-                # get user input in command line
-                menu_number = user_menu()
-
-                # make sure user login or create account before doing anything else
-                while myname == "" and menu_number not in [LIST_ACCOUNT, LOGIN, EXIT_PROGRAM,CREATE_ACCOUNT]:
-                    print("Please log in or create account first before doing other operations.")
-                    menu_number = user_menu()
-
-                # Further prompt users to input details for each function,
-                # Parse the input into command, and pass it to the socket
-                # when sending fails, server or client crashes or are off line
-                # for too long
-                try:
-                    DISPATCH[menu_number](s)
-                except:
-                    print("Send command operation failed, relog in and try again.")
-                    break # restarting socket, re log in and try again
+            # Further prompt users to input details for each function,
+            # Parse the input into command, and pass it to the socket
+            # when sending fails, server or client crashes or are off line
+            # for too long
+            try:
+                proceed_to_response = DISPATCH[menu_number](s)
+            except:
+                print("Send command operation failed, relog in and try again.")
+                proceed_to_response = False
+                s.close()
             
-                try:
-                    getResponse(s, menu_number)
-                except:
-                    print("Receive command operation failed, relog in and try again.")
-                    break
-               
-                # If client exits, no longer needs to tell the server to log off
-                if menu_number == EXIT_PROGRAM:
-                    sys.exit()
-
+            # handle response from the server
+            try:
+                if proceed_to_response:
+                #if menu_number not in (FETCH_MESSAGE_UI, LOGOUT_UI):
+                    get_response(s, menu_number)
+            except:
+                print("Receive command operation failed, relog in and try again.")
+            finally:
+                s.close()
--- clientFunction.py
+++ clientFunction.py
@@ -4,81 +4,17 @@ Created on Feb 16, 2023
 Author: Gary Ma, Tao Lin
 """
 from protocol import *
+myname = "" # global variable for the client program to know who I am
+mymsgcount = 1 # global variable for the client 
 
-
-####################################
-###### Handle Message Sent    ######
-####################################
-
-def client_send_message(msg_obj, socket):
-    """
-    Wrapper for client send message.
-    Return:
-        success or failure
-    Throw error if:
-        1) server socket.close/crash, error: connection terminated by the other side
-        2) Send all did not send all data: which error?
-        3) Time out: server taking too long to reply. timeout error
-    In all three cases, the connection is down and the upstream code should reinitialize client socket.
-    """
-    try:
-        msg_obj.send_to_socket()
-        return True
-    except:
-        print("Error: failed to send message.") # log in and try again
-        raise RuntimeError
-
-def create_account(socket):
-    """
-    User creating an account of alphabets and numeric of max length 12
-    Return:
-        true if account name is sent over to server
-        false if failed to send all messages to server
-    """
-    name = input("Input a username with alphabets and numeric of max length 12 >>").strip()
-    while not name.isalnum() or len(name)>12:
-        name = input("Input a username with alphabets and numeric of max length 12 >>").strip()
-
-    msg_obj = ClientMessage(op=CREATE_ACCOUNT, username=name)
-    # throw exception in the next line if send error encountered
-    success = client_send_message(msg_obj, socket)
-    return success
-
-def login_account(socket):
-    """
-    User log in some account after creating an account 
-    """
-    name = input("Input a user name to log in").strip()
-    while not name.isalnum() or len(name)>12:
-        name = input("Input a user name to log in").strip()
-    msg_obj = ClientMessage(op=LOGIN_ACCOUNT, username=name)
-    # throw exception in the next line if send error encountered
-    success = client_send_message(msg_obj, socket)
-    return sucess
-
-def list_account(socket):
-    """
-    List account name with wildcard
-    """
-    msg = input("Input a username search patter with alphabets, numeric and wildcard *").strip()
-    msg_obj = ClientMessage(op=LIST_ACCOUNT, message=msg)
-    success = client_send_message(msg_obj, socket)
-    return success
-
-def send_message(socket):
-    """
-    User send message to another user
-    """
-    msg = input("To which other use")
-
-DISPATCH = { CREATE_ACCOUNT: create_account,
-             LOGIN_ACCOUNT: login_account,
-             LIST_ACCOUNT: list_account,
-             SEND_MESSAGE: send_message,
-             DELETE_ACCOUNT: delete_account,
-             LOGOUT: logout,
-             FETCH_MESSAGE: fetch_message,
-             EXIST_PROGRAM: exist_program}
+CREATE_ACCOUNT_UI = 1
+LOGIN_ACCOUNT_UI = 2
+LIST_ACCOUNT_UI = 3
+SEND_MESSAGE_UI = 4
+DELETE_ACCOUNT_UI = 5
+LOGOUT_UI = 6
+FETCH_MESSAGE_UI = 7
+EXIT_PROGRAM_UI = 8
 
 ####################################
 ###### Handle Message Receipt ######
@@ -95,15 +31,14 @@ def client_receive_message(socket):
         3) receive zero byte -- receive_n_byte throws a runtime error
     In all three cases, the connection is down and the upstream code should reinitialize client socket.
     """
-    request = protocol.Message()
+    request = Message()
     try:
         request.receive_from_socket(socket)
-        print("Received from server:", request.op, request.status, request.username)
+        #print("Received from server:", request.op, request.status, request.username)
         return request
     except Exception as err:
         print("Error: failed to receive server message.") # log in and try again
-        raise RuntimeError    
-
+        raise RuntimeError
 
 
 # After sending a command to a server, receives a reply from the server
@@ -113,10 +48,184 @@ def get_response(socket, menu_number):
     msg_obj = client_receive_message(socket)
     
     # operation code has to be valid, else raise an error
-    if msg_obj.op not in DISPATH:
+    if msg_obj.op not in DISPATCH:
         print("Error: server returning operation {}, invalid!".format(msg_obj.op))
         raise RuntimeError
      
     # parsing info
     # For every operation, regardless of success or failure, message already encoded
     print(msg_obj.message)
+
+    # handle post response operations
+    # login failure ---- account does not exist yet
+    if menu_number == LOGIN_ACCOUNT_UI and msg_obj.status != SUCCESS:
+        global myname
+        myname = ""
+    elif menu_number == LOGIN_ACCOUNT_UI and msg_obj.status == SUCCESS:
+        print("Login success for account {}.".format(myname))
+    elif menu_number == DELETE_ACCOUNT_UI and msg_obj.status == SUCCESS:
+        print("Automatically logging out of account {}".format(myname))
+        logout(socket)
+
+
+
+####################################
+###### Handle Message Sent    ######
+####################################
+
+def client_send_message(msg_obj, socket):
+    """
+    Wrapper for client socket sending message to the server socket.
+    Return:
+        success or failure
+    Throw error if:
+        1) server socket.close/crash, error: connection terminated by the other side
+        2) Send all did not send all data: which error?
+        3) Time out: server taking too long to reply. timeout error
+    In all three cases, the connection is down and the upstream code should reinitialize client socket.
+    """
+    try:
+        msg_obj.send_to_socket(socket)
+        return True
+    except:
+        print("Error: failed to send message.") # log in and try again
+        raise RuntimeError
+
+def create_account(socket):
+    """
+    User creating an account of alphabets and numeric of max length 12
+    Return:
+        proceed to response true
+    """
+    name = input("Input a username with alphabets and numeric of max length {} >> ".format(USERNAME_LIMIT)).strip()
+    while not name.isalnum() or len(name) > USERNAME_LIMIT:
+        print("Username contains illegal character or is too long.")
+        name = input("Input a username with alphabets and numeric of max length {} >> ".format(USERNAME_LIMIT)).strip()
+
+    msg_obj = Message(op=CREATE_ACCOUNT, username=name)
+    # throw exception in the next line if send error encountered
+    client_send_message(msg_obj, socket)
+    return True
+
+def login_account(socket):
+    """
+    User log in an account after creating an account
+    It sends a request to server to verify if this account exists
+    The server won't store the log in information
+    This log in is only for the local program to store the global variable myname
+    return:
+        proceed to response true
+    """
+    name = input("Input a user name to log in >> ").strip()
+    while not name.isalnum() or len(name)> USERNAME_LIMIT:
+        print("Username contains illegal character or is too long.")
+        name = input("Input a user name to log in >> ").strip()
+
+    # ask the server if the supplied user name is in the list
+    msg_obj = Message(op=CHECK_ACCOUNT, username=name)
+    # throw exception in the next line if send error encountered
+    client_send_message(msg_obj, socket)
+    # regardless of server confirming the name, store this name for now
+    global myname
+    myname = name
+    return True
+
+
+def list_account(socket):
+    """
+    List account name with wildcard, only * is supported
+    """
+    msg = input("Input a username search pattern with alphabets, numeric and wildcard * >> ").strip()
+    while not msg.replace("*","").isalnum():
+        print("Username pattern contains illegal characters.")
+        msg = input("Input a username search patter with alphabets, numeric and wildcard * >> ").strip()
+
+    msg_obj = Message(op=LIST_ACCOUNT, message=msg)
+    # throw exception in the next line if send error encountered
+    success = client_send_message(msg_obj, socket)
+    return success
+
+
+def send_message(socket):
+    """
+    User send message to another user.
+    """
+    target_name = input("Specify a target name with alphabets and numeric of max length {} >> ".format(USERNAME_LIMIT)).strip()
+    while not target_name.isalnum() or len(target_name)> USERNAME_LIMIT:
+        target_name = input("Specify a target name with alphabets and numeric of max length {} >> ".format(USERNAME_LIMIT)).strip()
+    
+    msg = input("Input your message of max length {} characters >> ".format(MESSAGE_LIMIT))
+    while len(msg) > MESSAGE_LIMIT:
+        print("Message length longer than {}".format(MESSAGE_LIMIT))
+        msg = input("Input your message of max length {} characters >> ".format(MESSAGE_LIMIT))
+    msg_obj = Message(op=SEND_MESSAGE, username=myname, target_name=target_name, message=msg) 
+    # throw exception in the next line if send error encountered
+    success = client_send_message(msg_obj, socket)
+    return success
+
+def delete_account(socket):
+    """
+    A logged in user delete its account
+    This automatically logs out a user
+    return:
+        proceed to response. If no need to delete account, return false
+    """
+    msg = input("Are you sure to delete account {}(Y/N) >> ".format(myname))
+    while msg not in ["Y","N"]:
+        msg = input("Are you sure to delete account {}(Y/N) >> ".format(myname))
+
+    if msg == "N": return False
+    msg_obj = Message(op=DELETE_ACCOUNT, username=myname)
+    # throw exception in the next line if send error encountered
+    success = client_send_message(msg_obj, socket)
+    return True
+
+def logout(socket):
+    """
+    The server does not need to know a client is logged off.
+    Logging off here is only updating the global variable myname to ""
+    Return:
+        proceed to response: no need to listen to server's response
+    """
+    global myname
+    global mymsgcount
+    myname = ""
+    mymsgcount = 1
+    print("Log out successful.")
+    return False
+
+
+def fetch_message(socket):
+    """
+    The client keeps a counter mymsgcount for fetching through all historical msg.
+    When client/server crash, the counter mymsgcount restarts from 0 again. 
+    fetch message handles message receives itself.
+    return:
+        proceed to response: no need to listen to server's response
+    """
+    global mymsgcount
+    while True: # keep fetching messages until the last one
+        request = Message(FETCH_MESSAGE, mymsgcount, myname)
+        # throw exception in the next line if send error encountered
+        success = client_send_message(msg_obj, socket)
+        # receive from server how many more messages, throw exception in the next line if receive error encountered
+        response = client_receive_message(socket)
+        mymsgcount += 1
+        print("  ", response.username, ":", response.message)
+        
+        if response.status == NO_NEXT_MESSAGE:
+            print("Finished fetching all messages.")
+            break
+        # else status is NEXT_MESSAGE_EXIST
+    return False
+
+
+DISPATCH = { CREATE_ACCOUNT_UI: create_account,
+             LOGIN_ACCOUNT_UI: login_account,
+             LIST_ACCOUNT_UI: list_account,
+             SEND_MESSAGE_UI: send_message,
+             DELETE_ACCOUNT_UI: delete_account,
+             LOGOUT_UI: logout,
+             FETCH_MESSAGE_UI: fetch_message}
+
+
